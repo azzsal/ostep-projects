40c4e5c5ddf6605297a1779f67026ea124476ed1
azzsal <azzsal@protonmail.com>
2022-10-14 11:56:05 +0300

    Add solution to wgrep program

---
+++ initial-utilities/wgrep/wgrep.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int search_file(char *searchterm, FILE *fp);
+
+int main(int argc, char *argv[])
+{
+	if(argc == 1)
+	{
+		printf("wgrep: searchterm [file ...]\n");
+		exit(1);
+	}
+
+	if(argc == 2)
+	{
+		search_file(argv[1], stdin);
+		exit(0);
+	}
+
+	for(int i = 2; i < argc; i++)
+	{
+		FILE *fp = fopen(argv[i], "r");
+		if(!fp)
+		{
+			printf("wgrep: cannot open file\n");
+			exit(1);
+		}
+		search_file(argv[1], fp);
+	}
+
+	return 0;
+
+}
+
+int search_file(char *searchterm, FILE *fp)
+{
+	char *lineptr = NULL;
+	size_t len = 0;
+	while(getline(&lineptr, &len, fp) != -1)
+	{
+		if(strstr(lineptr, searchterm) != NULL)
+		{
+			printf("%s", lineptr);
+		}
+	}
+
+	free(lineptr);
+	fclose(fp);
+
+	return 0;
+}
